6cd0e25367c9f1d5663702961feafb9c951add90
yunlong <liuyunlong@huishoubao.com.cn>
2020-07-10 10:49:14 +0800

    chore: 添加store模板

--- template/src/main.js
+++ template/src/main.js
@@ -1,24 +1,17 @@
 import Vue from 'vue';
+import store from '@/store';
 import router from './router';
 import '@/common/less/common.less';
 
 <%- uiPlugin %>
 
-// 增加import引用第三方css方式，如果不用自定义，建议用static里面的css样式，通过script方式引用有利于缓存处理
-
 import App from './App.vue';
-// import store from './store';
 
 Vue.config.productionTip = false;
 // store.state.router = router;
 
-// import Ajax from 'utils/ajax'
-// Ajax.post({
-//     url: "/gateway/get_ft_list"
-// })
-
 new Vue({
     router,
-    // store,
+    store,
     render: h => h(App)
 }).$mount("#app")
---
+++ template/src/store/mutations.js
@@ -0,0 +1,16 @@
+export default {
+    setValue(state, param) {
+        const key = param.key;
+        if(!key) {
+            return;
+        }
+        const keyArr = key.toString().split('.');
+        let setParam = state;
+        for(var i in keyArr) {
+            if (i < keyArr.length - 1) {
+                setParam = setParam[keyArr[i]];
+            }
+        }
+        setParam[keyArr[keyArr.length - 1]] = param.value;
+    }
+};
