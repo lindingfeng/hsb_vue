f4177d9e828b8420a041d643d305ad71a7f6c9cd
yunlong <liuyunlong@huishoubao.com.cn>
2020-07-22 18:09:28 +0800

    chore: 添加文件名大小写检测插件

--- config/webpack.dev.config.js
+++ config/webpack.dev.config.js
@@ -1,10 +1,15 @@
 const path = require('path');
+const CaseSensitivePathsPlugin = require('case-sensitive-paths-webpack-plugin');
 const webpackConfig = require('./webpack.config');
 
 module.exports = (options) => {
   const baseConfig = webpackConfig(options);
   return {
     ...baseConfig,
+    plugins: [
+      ...baseConfig.plugins,
+      new CaseSensitivePathsPlugin(),
+    ],
     devServer: {
       contentBase: path.join(process.cwd(), './dist'),
       hot: true,
